136cd466f42b85627e66153d2bd2c0cf0539ee56
marcio <margeodev@gmail.com>
2017-06-25 17:14:23 -0300

    Criada a tela protótipo para o cadastro de pedidos

---
+++ main/Java/controller/CadastroPedidosBean.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidosBean {
+	
+	private List<Integer> itens;
+	
+	public CadastroPedidosBean(){
+		itens = new ArrayList<>();
+		for(int i = 0; i<50; i++){
+			itens.add(i);
+		}
+	}
+	
+	public List<Integer> getItens(){
+		return itens;
+	}
+}
