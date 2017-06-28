5fe90555a5d9a00cc90a44baaeb759449137a4eb
marcio <margeodev@gmail.com>
2017-06-28 15:59:52 -0300

    Criação da EntityManagerFactory

--- main/Java/controller/CadastroProdutoBean.java
+++ main/Java/controller/CadastroProdutoBean.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 
+import model.Categoria;
 import model.Produto;
+import repository.Categorias;
+import utils.jsf.FacesUtil;
 
 @Named
 @ViewScoped
@@ -13,7 +19,26 @@ public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Produto produto;
+	@Inject
+	private Categorias categorias;
+	
+	private Categoria categoriaPai;	
+	private Produto produto;	
+	private List<Categoria> categoriasRaizes;
+	private List<Categoria> subcategorias;
+	
+	
+	public void inicializar(){	
+		System.out.println("Inicializando...");
+		if(FacesUtil.isNotPostback()){			
+			categoriasRaizes = categorias.raizes();			
+		}
+		
+	}
+	
+	public void carregarSubcategorias(){
+		subcategorias = categorias.subcategoriasDe(categoriaPai);
+	}
 	
 	public CadastroProdutoBean() {
 		produto = new Produto();
@@ -26,4 +51,22 @@ public class CadastroProdutoBean implements Serializable {
 		return produto;
 	}
 
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	@NotNull
+	public Categoria getCategoriaPai() {
+		return categoriaPai;
+	}
+
+	public void setCategoriaPai(Categoria categoriaPai) {
+		this.categoriaPai = categoriaPai;
+	}
+	
+	@NotNull
+	public List<Categoria> getSubcategorias() {
+		return subcategorias;
+	}
+	
 }
